ff18d0af421532dc38424d23e8752baa2bfe49f2
EvanGlaizel <evanglaizel@outlook.com>
2024-02-08 20:19:32 -0500

    Created file

---
+++ solution6.py
@@ -0,0 +1,41 @@
+
+# takes in nAggressive, nPassive >= 0
+# returns the number of possible arrangements of dogs
+# Examples:
+# |  Input  |  Output  |
+# |---------|----------|
+# |   0,0   |     1    |
+# |---------|----------|
+# |   1,1   |     2    |
+# |---------|----------|
+# |   0,2   |     0    |
+# |---------|----------|
+# |   2,2   |    12    |
+# |---------|----------|
+def dogArrangements( nPassive, nAggressive ):
+    nFact = 1
+    kFact = 1
+
+    for i in range(1, nPassive + nAggressive):
+        nFact *= i
+
+    for i in range(1, nAggressive):
+        kFact *= i
+        
+
+    return nFact // kFact
+
+# Testing code provided in main():
+def main():
+    testArgs = [[0,0,1],[1,1,2],[0,2,0],[2,2,12]]
+    for arg in testArgs:
+        passive, aggressive, answer = arg
+        result = dogArrangements(passive,aggressive)
+        if result != answer:
+            print(f"Failed dogArrangements test with args nPassive={passive} nAggressive={aggressive}.\nExpected: {answer}, Got: {result}")
+        else:
+            print(f"Passed dogArrangements test with args nPassive={passive} nAggressive={aggressive}.")
+    return 0
+
+if __name__ == '__main__':
+    main()
